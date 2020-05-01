9e5bd9cb979710b552a34dca4e0fb45b6f5f4418
s.miyoshi <s.miyoshi1991@gmail.com>
2020-05-01 16:04:49 +0900

    show correct tags

--- src/components/video.js
+++ src/components/video.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { Tag } from './tag'
 import path from 'path'
+import { getTags } from '../plugins/db'
 
 export class Video extends Component {
   render() {
     return (
       <div>
         Title: {this.props.name}<br />
-        Tags: <Tag name="sample" /><br />
+        Tags: {getTags(this.props.name).map((tag) => <Tag name={tag} />)}<br />
         <video controls width="300px" src={path.resolve(this.props.name)}></video>
       </div>
     )
