0d8c95f80c8b332dbc1a5a4c18e19db06cf93851
s.miyoshi <s.miyoshi1991@gmail.com>
2020-05-02 16:44:55 +0900

    update tag remove

--- src/components/tag.js
+++ src/components/tag.js
@@ -2,17 +2,11 @@ import React, { Component } from 'react'
 import { removeTag } from '../plugins/db'
 
 export class Tag extends Component {
-  remove() {
-    console.log('remove tag: %s', this.props.name)
-    removeTag(this.props.file, this.props.name)
-    window.location.reload()
-  }
-
   render() {
     return (
       <div>
         {this.props.name}
-        <button onClick={() => this.remove()}>x</button>
+        <button onClick={() => this.props.remover(this.props.name)}>x</button>
       </div>
     )
   }
--- src/components/video.js
+++ src/components/video.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
 import { Tag } from './tag'
 import path from 'path'
-import { getTags } from '../plugins/db'
+import { getTags, removeTag } from '../plugins/db'
 
 export class Video extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { reload: false }
+    this.remover = this.remover.bind(this)
+  }
+
+  remover(tag) {
+    console.log('remove tag: %s', tag)
+    removeTag(this.props.name, tag)
+
+    // refresh component
+    this.setState(
+      { reload: true },
+      () => this.setState({ reload: false })
+    )
+  }
+
   render() {
     return (
       <div>
         Title: {this.props.name}<br />
-        Tags: {getTags(this.props.name).map((tag) => <Tag name={tag} file={this.props.name} />)}<br />
+        Tags: {getTags(this.props.name).map((tag) => <Tag name={tag} remover={this.remover} />)}<br />
         <video controls width="300px" src={path.resolve(this.props.name)}></video>
       </div>
     )
