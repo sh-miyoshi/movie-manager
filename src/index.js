25646d9b0e358ac2d65893f33b93a8853461ceb2
s.miyoshi <s.miyoshi1991@gmail.com>
2020-05-03 10:11:09 +0900

    install bootstrap

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { Video } from './components/video'
 import { getFilteredVideoFiles } from './plugins/db'
+import { Container, Row } from 'react-bootstrap'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        {
-          getFilteredVideoFiles().map((f) => <Video name={f}></Video>)
-        }
-      </div>
+      <Container>
+        <Row>TODO(headers)</Row>
+        <Row>TODO(selected tags)</Row>
+        <Row>
+          {
+            getFilteredVideoFiles().map((f) => <Video name={f}></Video>)
+          }
+        </Row>
+      </Container>
     )
   }
 }
